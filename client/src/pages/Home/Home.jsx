a982dc69b698ea9e3a7fd8de763ad0cf542db791
Zhaka-Hidayat <zhakazx@gmail.com>
2022-12-15 11:00:33 +0800

    feat: adding featured products, trending products, and categories section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
 import './Home.scss'
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <Categories />
+      <FeaturedProducts type="trending" />
     </div>
   )
 }
