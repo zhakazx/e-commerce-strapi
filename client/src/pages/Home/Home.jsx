f72096753010b610f652885724a739d0bdd3d059
Zhaka-Hidayat <zhakazx@gmail.com>
2022-12-15 12:37:44 +0800

    feat: adding contact section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 import './Home.scss'
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedProducts type="featured" />
       <Categories />
       <FeaturedProducts type="trending" />
+      <Contact />
     </div>
   )
 }
